5753f7fdf8f74fe8b85441279cecf85b88d9bedc
Maninder Singh <manny.singh.live@gmail.com>
2023-06-02 21:27:55 -0400

    TodoCard UI in working order

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,4 +1,6 @@
-import { Draggable } from "react-beautiful-dnd"
+import { Draggable, Droppable } from "react-beautiful-dnd"
+import TodoCard from "./TodoCard"
+import { PlusCircleIcon } from "@heroicons/react/24/solid"
 
 type Props = {
     id: TypedColumn,
@@ -6,18 +8,83 @@ type Props = {
     index: number
 }
 
+const idToColumnText : {
+    [key in TypedColumn] : string
+} = {
+    "todo" : "To Do",
+    "inprogress" : "In Progress", 
+    "done" : "Done"
+}
+
 function Column({ id, todos, index} : Props) {
   return (
-    <Draggable>
-        {(provided) => {
-            <div
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                ref={provided.innerRef}
-            >
-                {/* render droppable todos in the column  */}
-            </div>
-        }}
+    <Draggable draggableId={id} index={index}>
+        {
+            (provided) => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                > 
+                    
+                    <Droppable droppableId={index.toString()} type="card">
+                        {
+                            (provided, snapshot) => (
+                                <div
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                    className={`p-2 rounded-2xl shadow-sm ${snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"}`}
+                                >
+                                    <h2 className="font-bold justify-between p-2 text-xl flex">
+                                        {idToColumnText[id]}
+                                        <span 
+                                            className="text-gray-500 bg-gray-200 rounded-full font-normal text-xs px-2 py-1 items-center"
+                                        >
+                                            {todos.length}
+                                        </span>    
+                                    </h2>
+
+                                    <div className="space-y-2">
+                                        {
+                                            todos.map((todo, index) => (
+                                                <Draggable
+                                                    key={todo.$id}
+                                                    draggableId={todo.$id}
+                                                    index={index}
+                                                >
+                                                    {
+                                                        (provided) => (
+                                                            <TodoCard 
+                                                                todo={todo}
+                                                                index={index}
+                                                                id={id}
+                                                                innerRef={provided.innerRef}
+                                                                draggableProps={provided.draggableProps}
+                                                                dragHandleProps={provided.dragHandleProps}
+                                                            />
+                                                        )
+                                                    }
+                                                </Draggable>
+                                            ))
+                                        }
+
+                                        {provided.placeholder}
+
+                                        <div className="flex items-end justify-end">
+                                            <button>
+                                                <PlusCircleIcon 
+                                                    className="h-10 w-10 text-green-500 hover:text-green-600"
+                                                />
+                                            </button>
+                                        </div>  
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </Droppable>
+                </div>
+            )
+        }
     </Draggable>
   )
 }
